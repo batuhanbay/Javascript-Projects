7e7ceaf853af85a26933265785a98f8ee262970b
mustafa batuhan bayoglu <bayoglubatuhan@gmail.com>
2020-11-30 17:48:05 +0300

    Convert to ES6 from ES5 and add comment

--- BMI Calculator/calculator.js
+++ BMI Calculator/calculator.js
@@ -5,12 +5,11 @@ app.use(bodyParser.urlencoded({extended : true}));
 
 
 
-app.get("/",function(req, res){
-
-res.sendFile(__dirname + "/index.html");
+app.get("/",(req, res) =>{
+  res.sendFile(__dirname + "/index.html"); //being sent to html file and render which is index.html
 });
 
-app.post("/", function(req,res){
+app.post("/",(req,res) =>{ // If there is post method and targetting from route page to server , implement to post method and send it back to route
 
   var num1 = Number(req.body.num1);
   var num2 = Number(req.body.num2);
@@ -20,15 +19,16 @@ app.post("/", function(req,res){
   res.send("The result of the calculation is " + result);
 });
 
-app.get("/bmicalculator", function(req, res){
+app.get("/bmicalculator", (req, res) => { //being sent to html file and render which is bmiCalculator.html
 
   res.sendFile(__dirname + "/bmiCalculator.html");
   
 });
 
-app.post("/bmicalculator", function(req, res){
+app.post("/bmicalculator",(req, res) =>{ // If there is post method and targetting from /bmicalculator page to server , 
+                                          //implement to post method and send it back to /bmicalculator
 
-  var weight = parseFloat(req.body.weight);
+  var weight = parseFloat(req.body.weight); //req.body thanks to body-parser package
   var height = parseFloat(req.body.height);
 
   var bmi = weight / (height * height);
@@ -36,6 +36,6 @@ app.post("/bmicalculator", function(req, res){
   res.send("The result of the BMI calculation: " + bmi);
 });
 
-app.listen(3000,function(){
+app.listen(3000,() => {
   console.log("Server started on port 3000!");
 });
