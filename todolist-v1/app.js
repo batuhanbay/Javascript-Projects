d6aeed3ecb62957b8562086d48a8a81dcf592ed4
mustafa batuhan bayoglu <bayoglubatuhan@gmail.com>
2020-12-10 22:23:45 +0300

    Implement EJS to project

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -5,12 +5,45 @@ const bodyParser = require("body-parser");
 const app = express();
 
 
+app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({extended:true}));
+
+const today = new Date();
+const currentDay = today.getDay();
+let day = "";
 
 app.get("/", (req, res) =>{
 
-  const today = new Date();
-  today.getDay() === 6 || today.getDay() === 0 ? res.sendFile(`${__dirname}/weekend.html`) : res.sendFile(`${__dirname}/weekday.html`);
+  
+  switch (currentDay) {
+    case 0:
+      day = "Sunday"
+      break;
+    case 1:
+      day = "Monday"
+      break;
+    case 2:
+      day = "Tuesday"
+      break;
+    case 3:
+      day = "Weednesday"
+      break;
+    case 4:
+      day = "Thursday"
+      break;
+    case 5:
+      day = "Friday"
+      break;
+    case 6:
+      day = "Saturday"
+      break;
+  
+    default:
+      console.log("There is something went wrong!");
+      break;
+  }
+  res.render("list", {kindOfDay: day})
 
 }); 
 
