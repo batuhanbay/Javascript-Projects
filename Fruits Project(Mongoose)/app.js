c231a5a2666323e0ad8f92e337fb620aa31c9ee9
mustafa batuhan bayoglu <bayoglubatuhan@gmail.com>
2021-01-01 22:46:17 +0300

    Progress on the mongoose.

--- Fruits Project(Mongoose)/app.js
+++ Fruits Project(Mongoose)/app.js
@@ -21,3 +21,115 @@ const fruit = new Fruit({ //This is going to be a new Fruit and this is to show 
 });
 
 fruit.save();//And this calls the save method in Mongoose to save this fruit document into a fruit collection inside our fruitsDB.
+
+// Relationships and embedding documents
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  favouriteFruit: fruitSchema,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const pineapple = new Fruit({
+  name: "Pineapple",
+  score: 9,
+  review: "Great fruit.",
+});
+
+// pineapple.save();
+
+const person = new Person({
+  name: "John",
+  age: 37,
+  favouriteFruit: pineapple,
+});
+
+// person.save();
+
+const mango = new Fruit({
+  name: "Mango",
+  score: 6,
+  review: "Decent fruit.",
+});
+
+// mango.save();
+
+Person.updateOne({ name: "John" }, { favouriteFruit: mango }, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Succesfully updated the documents");
+  }
+});
+
+// Create many documents
+const kiwi = new Fruit({
+  name: "Kiwi",
+  rating: 10,
+  review: "The best fruit!",
+});
+
+const orenge = new Fruit({
+  name: "orenge",
+  rating: 4,
+  review: "Too sour for me",
+});
+
+const banana = new Fruit({
+  name: "Banana",
+  rating: 3,
+  review: "Weird texture",
+});
+
+// Fruit.insertMany([kiwi, orenge, banana], (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Succesfully saved all the fruits to fruitDB");
+//   }
+// });
+
+// Read all documents
+Fruit.find((err, fruits) => {
+  if (err) {
+    console.log(err);
+  } else {
+    mongoose.connection.close();
+    console.log(fruits);
+    fruits.forEach((element) => {
+      console.log(element.name);
+    });
+  }
+});
+
+// Update one document
+Fruit.updateOne(
+  { _id: "5fea43a67013833d80e9aec7" },
+  { name: "Peach" },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Succesfully updated the document.");
+    }
+  }
+);
+
+// Delete one document
+// Fruit.deleteOne({ _id: "5fea43a67013833d80e9aec7" }, (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Succesfully deleted the document.");
+//   }
+// });
+
+// Delete all documents
+// Person.deleteMany({ name: "John" }, (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Succesfully deleted all the document.");
+//   }
+// });
