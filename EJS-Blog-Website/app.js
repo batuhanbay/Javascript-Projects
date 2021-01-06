bcdb255e3dda431e9d0591ce97deb9a3f189cca5
mustafa batuhan bayoglu <bayoglubatuhan@gmail.com>
2021-01-07 02:19:46 +0300

    Connet project to Mongo Database

--- EJS-Blog-Website/app.js
+++ EJS-Blog-Website/app.js
@@ -4,72 +4,85 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+const homeStartingContent =
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent =
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent =
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let posts = [];
-
-app.get("/", (req,res) =>{
-  res.render("home",{
-    startingContent: homeStartingContent,
-    posts:posts
+app.get("/", (req, res) => {
+  Post.find({}, (err, posts) => {
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts,
+    });
   });
 });
 
-app.get("/about", (reg,res) =>{
-
-  res.render("about",{
-    aboutPageContent: aboutContent
-  })
-
+app.get("/about", (reg, res) => {
+  res.render("about", {
+    aboutPageContent: aboutContent,
+  });
 });
 
-app.get("/contact", (req,res) =>{
+app.get("/contact", (req, res) => {
   res.render("contact", {
-    contactPageContent:contactContent
-  })
+    contactPageContent: contactContent,
+  });
 });
 
-app.get("/compose",(req,res) =>{
+app.get("/compose", (req, res) => {
   res.render("compose");
 });
 
-app.post("/compose",(req,res) =>{
-
-  const post = { 
-    title:req.body.postTitle,
-    content:req.body.postBody
-  };
-  posts.push(post);
-  res.redirect("/");
-});
-
-app.get("/posts/:postName",(req,res) =>{
-
-  const requestedTitle = _.lowerCase(req.params.postName)
-  
-  posts.forEach(post => {
-    const storedTitle = _.lowerCase(post.title);
-    storedTitle === requestedTitle ? res.render("post",{
-      title: post.title,
-      content: post.content
-    }): console.log("Matches not found.") ;
+app.post("/compose", (req, res) => {
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody,
   });
-  
 
+  post.save((err) => {
+    //save method works async , Redirecting in the callback of the save() funtion ensures that it happens after a successful insert.
+    !err ? res.redirect("/") : console.log("Unsuccesffuly process.");
+  });
 });
 
+app.get("/posts/:postId", function (req, res) {
+  const requestedPostId = req.params.postId;
 
-app.listen(3000, function() {
+  Post.findById(requestedPostId, (err, post) => {
+    if (err) return next(err);
+
+    res.render("post", {
+      title: post.title,
+
+      content: post.content,
+    });
+  });
+});
+
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
