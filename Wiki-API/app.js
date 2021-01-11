e5056c68cba0d95ee76110ce2b9cea0446ef60bc
mustafa batuhan bayoglu <bayoglubatuhan@gmail.com>
2021-01-11 23:52:16 +0300

    Complete the put , patch and delete a specific article.

--- Wiki-API/app.js
+++ Wiki-API/app.js
@@ -55,7 +55,34 @@ app.route("/articles/:articleTitle")
   Article.findOne({title: req.params.articleTitle}, (err, foundArticle)=>{
     (foundArticle) ? res.send(foundArticle) : res.send("No articles matching that title was found.");
   });
-}); 
+})
+.put((req,res) =>{
+  Article.update(
+    {title: req.params.articleTitle},
+    {title: req.body.title, content: req.body.content},
+    {overwrite: true},
+    (err)=>{
+      (!err) ? res.send("Succesfully updated.") : res.send("Unsuccesfully could not updated.");
+    }
+  );
+})
+.patch((req,res) =>{
+  Article.update(
+    {title:req.params.articleTitle},
+    {$set: req.body},
+    (err) =>{
+      (!err) ? res.send(`Succesfully updated.`) : res.send(err);
+    } 
+  )
+})
+.delete((req,res) =>{
+  Article.deleteOne(
+    {title: req.params.articleTitle},
+    (err) =>{
+      (!err) ? res.send(`Succesfully ${req.params.articleTitle} deleted.`) : res.send(err);
+    }
+  );
+});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
