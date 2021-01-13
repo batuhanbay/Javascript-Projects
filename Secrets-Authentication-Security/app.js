819d53986d83688772c26c0d2a46af2d7eb7a763
mustafa batuhan bayoglu <bayoglubatuhan@gmail.com>
2021-01-13 14:43:48 +0300

    Implement salting and hashing passwords with bcrypt

--- Secrets-Authentication-Security/app.js
+++ Secrets-Authentication-Security/app.js
@@ -3,8 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
-
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -20,52 +20,58 @@ app.use(bodyParser.urlencoded({
 
 }));
 
-mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String  
+  password: String
 })
 
 const User = mongoose.model("User", userSchema);
 
-app.get("/", (req,res) =>{
+app.get("/", (req, res) => {
   res.render("home");
 });
-app.get("/login", (req,res) =>{
+app.get("/login", (req, res) => {
   res.render("login");
 });
-app.get("/register", (req,res) =>{
+app.get("/register", (req, res) => {
   res.render("register");
 });
 
-app.post("/register",(req,res) =>{
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
+app.post("/register", (req, res) => {
+
+  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(err => {
+      (!err) ? res.render("secrets") : console.log("Could not save succesfully ");
+    });
   });
-  newUser.save(err =>{
-    (!err) ? res.render("secrets") : console.log("Could not save succesfully ");
-  });
+
 });
 
-app.post("/login", (req,res) =>{
+app.post("/login", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-  User.findOne({email: req.body.username}, (err, foundUser) =>{
-    if(err){
+  User.findOne({ email: req.body.username }, (err, foundUser) => {
+    if (err) {
       console.log(err);
     }
-    else{
-      if(foundUser){
-        if(foundUser.password = password){
-          res.render("secrets");
-        }
+    else {
+      if (foundUser) {
+        // Load hash from your password DB.
+        bcrypt.compare(password, foundUser.password, function (err, result) {
+          // result == true
+          (result) ? res.render("secrets") : res.send(result);
+        });
       }
     }
   });
 });
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
